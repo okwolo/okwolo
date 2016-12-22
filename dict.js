c9a3b2170b74511e0fd12ef4681b32a892276f04
Gabriel <pabombs@gmail.com>
2016-12-21 23:06:11 -0500

    main lib now a reducer + state management wrapper

--- dict.js
+++ dict.js
@@ -1,25 +1,41 @@
 // returns a state wrapper which be read, and modified through actions
-var dict = function(initial_state, action_types, middleware, watchers) {
+var dict = function(action_types, middleware, watchers) {
 
-    // TODO check all args are defined
-
-    // set current state
-    var current_state = Object.assign({}, initial_state);
+    // default values for all arguments
+    if (action_types === undefined) {
+        action_types = [{}];
+    }
+    if (middleware === undefined) {
+        middleware = [];
+    }
+    if (watchers === undefined) {
+        watchers = [];
+    }
 
     // make sure action_types is an array
     if (!Array.isArray(action_types)) {
         action_types = [action_types];
     }
-    
+
     // make sure all action types have required valid properties
     action_types.forEach(function(current_action_types, i) {
         Object.keys(current_action_types).forEach(function(current_action_type, j) {
+            if (typeof current_action_types[current_action_type] === 'function') {
+                current_action_types[current_action_type] = {
+                    target: [],
+                    do: current_action_types[current_action_type]
+                }
+            }
             if (!Array.isArray(current_action_types[current_action_type])) {
                 current_action_types[current_action_type] = [current_action_types[current_action_type]];
             }
             current_action_types[current_action_type].forEach(function(current_action, k) {
-                if (!(Array.isArray(current_action.target) && current_action.target.length > 0)) {
-                    throw new Error(`Target of action ${k} of type ${current_action_type} in action types ${i} is invalid`);
+                if (current_action.target === undefined) {
+                    current_action.target = [];
+                } else if (typeof current_action.target === 'string') {
+                    current_action.target = [current_action.target];
+                } else if (!Array.isArray(current_action.target)) {
+                    throw new Error(`Target of action ${k} of type ${current_action_type} in action types ${i} is not an array`);
                 }
                 if (typeof current_action.do !== 'function') {
                     throw new Error(`Property "do" of action ${k} of type ${current_action_type} in action types ${i} is not a function`);
@@ -57,24 +73,23 @@ var dict = function(initial_state, action_types, middleware, watchers) {
         }
     });
 
-    // return copy of state
-    function get() {
-        return Object.assign({}, current_state);
-    }
-
-    // execute() wrapper that applies middleware and inkoes calls watchers
-    function act(type, params) {
-        var state = Object.assign({}, current_state);
+    // execute() wrapper that applies middleware and calls watchers
+    function act(state, type, params) {
+        var state = Object.assign({}, state);
+        if (params === undefined) {
+            params = {};
+        }
         var funcs = [execute];
         middleware.reverse().forEach(function(current_middleware, index) {
             funcs[index+1] = (function(state, type, params) {
                 return current_middleware(funcs[index], state, type, params);
             });
         });
-        current_state = Object.assign({}, funcs[middleware.length](state, type, params));
+        state = Object.assign({}, funcs[middleware.length](state, type, params));
         watchers.forEach(function(watcher) {
-            watcher(get(), type, params);
+            watcher(Object.assign({}, state), type, params);
         });
+        return state;
     }
 
     // exectute an action on the state
@@ -83,12 +98,16 @@ var dict = function(initial_state, action_types, middleware, watchers) {
         action_types.forEach(function(current_action_types, i) {
             var action = current_action_types[type];
             if (!action) {
-                throw new Error('action not defined');
+                return;
             }
             action.forEach(function(current_action) {
                 var target = Object.assign({}, local_state);
                 current_action.target.forEach(function(key, k) {
-                    target = target[key]||{};
+                    if (target[key] !== undefined) {
+                        target = target[key];
+                    } else {
+                        target = {};
+                    }
                 });
                 target = current_action.do(target, params);
                 for (var i = current_action.target.length-1; i >= 0; --i) {
@@ -104,74 +123,5 @@ var dict = function(initial_state, action_types, middleware, watchers) {
 
     return {
         act: act,
-        get: get
     }
 }
-
-// starting state
-var s = {
-    total: 0,
-    test: {
-        test: [1,2,3]
-    }
-};
-
-// definition of all actions
-var a = [{
-    ADD: [
-        {
-            target: ['total'],
-            do: function(target, params) {
-                return target + params;
-            }
-        },
-        {
-            target: ['test', 'test'],
-            do: function(target, params) {
-                return target.map((val) => val+params);
-            }
-        }
-    ],
-    MULT: [{
-        target: ['test'],
-        do: function(target, params) {
-            return {
-                test: target.test.map((val) => val*params.val)
-            }
-        }
-    }]
-}];
-
-// list of all middleware (executed top first)
-var m = [
-    function(callback, state, type, params) {
-        console.log('before');
-        var temp = callback(state, type, params);
-        console.log('after');
-        return temp;
-    },
-    // logging state and action
-    function(callback, state, type, params) {
-        console.log('state > %c%s', 'color:#a00;', JSON.stringify(state));
-        console.log('%c%s', 'font-size:20px;', type + ' ' + JSON.stringify(params));
-        var temp = callback(state, type, params);
-        console.log('state > %c%s', 'color:#0a0;', JSON.stringify(temp));
-        return temp;
-    }
-];
-
-// watcher function
-var w = function(state, type, params) {
-    console.log('%cI\'m watching', 'font-style:italic;font-family:"Comic Sans MS";color:#00f;font-size:50px;');
-}
-
-// create store
-var test_store = dict(s, a, m, w);
-
-// attach actions to user events
-window.addEventListener('click', function() {
-    test_store.act('MULT', {val:2});
-});
-window.addEventListener('keydown', function() {
-    test_store.act('ADD', 2);
-});
---
+++ dict_time_machine_test.js
@@ -0,0 +1,47 @@
+(function() {
+
+    var state = {
+        number: 0,
+        otherval: 'a'
+    }
+
+    var actions = {
+        ADD: function(target, params) {
+            target.number += params;
+            return target;
+        },
+        SUB: function(target, params) {
+            target.number -= params;
+            return target;
+        },
+        MUL: function(target, params) {
+            target.number *= params;
+            return target;
+        },
+        DIV: function(target, params) {
+            target.number /= params;
+            return target;
+        }
+    }
+
+    var test_time_machine = dict_time_machine(state, actions, undefined, function() {
+        console.log('watched');
+    });
+
+    window.addEventListener('keydown', function(e) {
+        if (e.key === 'z') {
+            test_time_machine.act('UNDO');
+        } else if (e.key === 'x') {
+            test_time_machine.act('REDO');
+        } else if (e.key === '+') {
+            test_time_machine.act('ADD', 1);
+        } else if (e.key === '-') {
+            test_time_machine.act('SUB', 1);
+        } else if (e.key === '*') {
+            test_time_machine.act('MUL', 2);
+        } else if (e.key === '/') {
+            test_time_machine.act('DIV', 2);
+        }
+    });
+
+}())
