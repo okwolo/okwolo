bb29c05ca5bb354e2f97662b3459acc65afcc36c
Gabriel <pabombs@gmail.com>
2017-04-01 01:47:22 -0400

    added error catching to goo-state

--- goo-state/goo-state.js
+++ goo-state/goo-state.js
@@ -1,71 +1,94 @@
-const utils = require('goo-utils');
+const {assert, deepCopy, isDefined, isArray, isFunction, isString} = require('goo-utils');
 
 // creates an object that acts on a state
 let state = () => {
     let actions = {};
+    let formatters = [];
     let middleware = [];
     let watchers = [];
+
+    let addAction = (action) => {
+        let formattedAction = formatters.reduce((acc, formatter) => {
+            return formatter(acc);
+        }, action);
+        assert(isString(formattedAction.type), `action type ${formattedAction.type} is not a string`);
+        assert(isFunction(formattedAction.handler), `handler for action ${formattedAction.type} is not a function`);
+        assert(isArray(formattedAction.target), `target of action ${formattedAction.type} is not an array`);
+        formattedAction.target.forEach((address) => {
+            assert(isString(address), `target of action type ${formattedAction.type} is not an array of strings ${formattedAction.target}`);
+        });
+        if (actions[formattedAction.type] === undefined) {
+            action[formattedAction.type] = [formattedAction];
+        } else {
+            action[type].push(formattedAction);
+        }
+    };
+
+    let addActionFormatter = (formatter) => {
+        let {handler, position} = formatter;
+        assert(isFunction(handler), `action formatter is not a function\n${handler}`);
+        if (position !== false) {
+            formatters.push(handler);
+        } else {
+            formatters.unshift(handler);
+        }
+    };
+
+    let addMiddleware = (middleware) => {
+        let {handler, position} = middleware;
+        assert(isFunction(handler), `middleware is not a function\n${handler}`);
+        if (position !== false) {
+            middleware.push(handler);
+        } else {
+            middleware.unshift(handler);
+        }
+    };
+
+    let addWatcher = (watcher) => {
+        let {handler, position} = watcher;
+        assert(isFunction(handler), `watcher is not a function\n${handler}`);
+        if (position !== false) {
+            watcher.push(handler);
+        } else {
+            watcher.unshift(handler);
+        }
+    };
 
     // supported blobs and their execution
     let use = (blob) => {
         let blobs = {
-            action: () => {
-                let {type, handler} = blob.action;
-                if (actions[type] === undefined) {
-                    action[type] = [handler];
-                } else {
-                    action[type] = handler;
-                }
-            },
-            middleware: () => {
-                let {func, position} = blob.middlware;
-                if (position !== false) {
-                    middleware.push(func);
-                } else {
-                    middleware.unshift(func);
-                }
-            },
-            watcher: () => {
-                let {func, position} = blob.middlware;
-                if (position !== false) {
-                    watcher.push(func);
-                } else {
-                    watcher.unshift(func);
-                }
-            },
+            action: addAction(blob.action),
+            formatter: addActionFormatter(blob.formatter),
+            middleware: addMiddleware(blob.middleware),
+            watcher: addWatcher(blob.watcher),
         };
         (blobs[Object.keys(blob)[0]] || (() => {}))();
     };
 
     // exectute an action on the state
     let execute = (state, type, params) => {
-        let newState = utils.deepCopy(state);
-        if (actions[type] === undefined) {
-            utils.err(`action type '${type}' was not found`);
-        }
+        let newState = deepCopy(state);
+        assert(isDefined(actions[type]), `action type '${type}' was not found`);
         actions[type].forEach((currentAction) => {
-            let target = utils.deepCopy(newState);
+            let target = deepCopy(newState);
             if (currentAction.target.length > 0) {
                 let reference = newState;
                 currentAction.target.forEach((key, i, a) => {
-                    if (target[key] !== undefined) {
-                        if (i === a.length - 1) {
-                            reference[key] = currentAction.do(target[key], params);
-                        } else {
-                            target = target[key];
-                            reference = reference[key];
-                        }
+                    assert(isDefined(target[key]), `target address of action ${type} is invalid: @state.${currentAction.target.join('.')}`);
+                    if (i === a.length - 1) {
+                        reference[key] = currentAction.handler(target[key], params);
                     } else {
-                        utils.err(`target address of action ${type} is invalid: @state.${currentAction.target.join('.')}`);
+                        target = target[key];
+                        reference = reference[key];
                     }
                 });
             } else {
-                newState = currentAction.do(target, params);
+                newState = currentAction.handler(target, params);
             }
         });
 
         watchers.forEach((watcher) => {
-            watcher(utils.deepCopy(newState), type, params);
+            watcher(deepCopy(newState), type, params);
         });
     };
 
@@ -80,10 +103,10 @@ let state = () => {
             funcs[index + 1] = (_state, _type = type, _params = params) => {
                 type = _type;
                 params = _params;
-                currentMiddleware(funcs[index], utils.deepCopy(_state), _type, _params);
+                currentMiddleware(funcs[index], deepCopy(_state), _type, _params);
             };
         });
-        funcs[middleware.length](utils.deepCopy(state), type, params);
+        funcs[middleware.length](deepCopy(state), type, params);
     };
 
     return {
--- goo-utils/goo-utils.js
+++ goo-utils/goo-utils.js
@@ -12,32 +12,34 @@ let utils = () => {
 
     // displays error message
     let err = (message) => {
-        throw(new Error(`** ${message}`));
+        throw(new Error(`gooErr:: ${message}`));
     };
 
-    let checkThat = (value) => {
-        let isDefined = () => value !== undefined;
-
-        let isArray = () => Array.isArray(value);
-
-        let isFunction = () => typeof value === 'function';
-
-        let isNode = () => value instanceof Node;
-
-        return {
-            isDefined: isDefined,
-            isArray: isArray,
-            isFunction: isFunction,
-            isNode: isNode,
-        };
+    // throw errors when assertion fails
+    let assert = (result, message) => {
+        if (result === false) {
+            err(message);
+        }
     };
+
+    // type checks
+    let isDefined = () => value !== undefined;
+    let isArray = () => Array.isArray(value);
+    let isFunction = () => typeof value === 'function';
+    let isString =() => typeof myVar === 'string';
+    let isNode = () => value instanceof Node;
 
     // public interface
     return {
         jsonCopy: jsonCopy,
         deepCopy: deepCopy,
         err: err,
-        checkThat: checkThat,
+        assert: assert,
+        isDefined: isDefined,
+        isArray: isArray,
+        isFunction: isFunction,
+        isString: isString,
+        isNode: isNode,
     };
 };
 
