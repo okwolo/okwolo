438e1c5a366778ad21f848d8f362235bbcd91882
Gabriel <pabombs@gmail.com>
2017-06-25 00:39:59 -0400

    added gzip plugin to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const CompressionPlugin = require('compression-webpack-plugin');
+
 module.exports = {
     entry: './goo-js/goo.js',
     output: {
@@ -18,4 +20,10 @@ module.exports = {
             },
         ],
     },
+    plugins: [
+        new CompressionPlugin({
+            asset: '[path].gz',
+            test: /goo\.min\.js$/,
+        }),
+    ],
 };
