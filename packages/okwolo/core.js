4eecb4f3cedf2bd999a8caccb7f35c00a4ab7e52
--global <gabrielj.harel@gmail.com>
2017-10-08 21:23:01 -0400

    added comments to core

--- packages/okwolo/core.js
+++ packages/okwolo/core.js
@@ -2,10 +2,15 @@
 
 const {isFunction, isDefined, assert, deepCopy, isBrowser, makeBus} = require('@okwolo/utils')();
 
+// version cannot be taken from package.json because environment is not guaranteed.
 const version = '1.3.0';
 
 const core = ({modules, blobs, options}) => {
+    // if it is needed to define the window but not yet add a target, the first
+    // argument can be set to undefined.
     const okwolo = (target, _window) => {
+        // if the kit requires the browser api, there must be a window object in
+        // scope or a window object must be injected as argument.
         if (options.browser) {
             if (!isDefined(_window)) {
                 assert(isBrowser(), 'app : must be run in a browser environment');
@@ -15,19 +20,26 @@ const core = ({modules, blobs, options}) => {
 
         const emit = makeBus();
         const use = makeBus();
+
+        // the api object will contain all the exposed functions/variables.
         const api = {emit, use};
 
+        // each module is instantiated.
         modules.forEach((_module) => {
             _module({emit, use}, _window);
         });
 
+        // each blob is used.
         blobs.forEach((blob) => {
             use(blob(_window));
         });
 
+        // reference to initial state is kept to be able to track whether it
+        // has changed using strict equality.
         const initial = {};
         let _state = initial;
 
+        // current state is monitored and stored.
         emit.on('state', (newState) => {
             _state = newState;
         });
@@ -37,11 +49,15 @@ const core = ({modules, blobs, options}) => {
             return deepCopy(_state);
         };
 
+        // the only functionality from the dom module that is directly exposed
+        // is the update event.
         if (options.modules.dom) {
             api.update = () => {
                 emit({state: _state});
             };
 
+            // target is used if it is defined, but this step can be deferred
+            // if it is not convenient to pass the target on app creation.
             if (isDefined(target)) {
                 use({target});
             }
@@ -49,10 +65,14 @@ const core = ({modules, blobs, options}) => {
 
         if (options.modules.state) {
             api.act = (type, params) => {
-                assert(type === 'SET_STATE' || _state !== initial, 'act : cannot act on state before it has been set');
+                // the only action that does not need the state to have already
+                // been changed is SET_STATE
+                assert(_state !== initial || type === 'SET_STATE', 'act : cannot act on state before it has been set');
                 emit({act: {state: _state, type, params}});
             };
 
+            // action is used to override state in order to give visibility to
+            // watchers and middleware.
             use({action: {
                 type: 'SET_STATE',
                 target: [],
@@ -61,29 +81,32 @@ const core = ({modules, blobs, options}) => {
 
             api.setState = (replacement) => {
                 if (isFunction(replacement)) {
-                    api.act('SET_STATE', replacement(deepCopy(_state)));
+                    api.act('SET_STATE', replacement(_state));
                     return;
                 }
                 api.act('SET_STATE', replacement);
             };
-
-            if (options.modules.history) {
-                api.undo = () => {
-                    api.act('UNDO');
-                };
-
-                api.redo = () => {
-                    api.act('REDO');
-                };
-            }
         } else {
-            assert(!options.modules.history, 'app : cannot use history blob without the state module', options);
+            // new state is emitted directly instead of giving that responsibility
+            // to the state module.
             api.setState = (replacement) => {
                 if (isFunction(replacement)) {
                     emit({state: replacement(deepCopy(_state))});
                     return;
                 }
                 emit({state: replacement});
+            };
+        }
+
+        if (options.modules.history) {
+            assert(options.modules.state, 'app : cannot use history blob without the state module', options);
+
+            api.undo = () => {
+                api.act('UNDO');
+            };
+
+            api.redo = () => {
+                api.act('REDO');
             };
         }
 
@@ -96,6 +119,8 @@ const core = ({modules, blobs, options}) => {
                 emit({show: {path, params}});
             };
 
+            // first argument can be a path string to register a route handler
+            // or a function to directly use a builder.
             api.register = (path, builder) => {
                 if (isFunction(path)) {
                     use({builder: path()});
@@ -115,9 +140,14 @@ const core = ({modules, blobs, options}) => {
             };
         }
 
+        // the returned object contains all the keys added to the api object
+        // but is also the register function.
         return Object.assign(api.register, api);
     };
 
+    // okwolo attempts to define itself globally and includes information about
+    // the version number and kit name. note that different kits can coexist,
+    // but not two kits with the same name and different versions.
     if (isBrowser()) {
         okwolo.kit = options.kit;
         okwolo.version = version;
