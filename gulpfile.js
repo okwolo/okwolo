e4f4095d4cbb0706e357163d361bb6fca2c564ca
--global <gabrielj.harel@gmail.com>
2017-10-26 19:30:48 -0400

    closes #100

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,21 @@
 
 const gulp = require('gulp');
 const del = require('del');
+const globby = require('globby');
+
+const kitPattern = './src/kits/*.js';
+
+gulp.task('prepublish', () => {
+    gulp.src(globby.sync([kitPattern]))
+        .pipe(gulp.dest('./'));
+});
 
 gulp.task('clean', () => {
+    const kits = globby.sync([kitPattern])
+        .map((path) => path.replace(/.+?(\w+\.js)$/g, '$1'));
     return del([
-        './okwolo.js',
-        './okwolo.min.js',
+        ...kits,
+        'okwolo*.js',
         './coverage',
         './npm-debug.log',
     ]);
